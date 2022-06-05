32b92a7ad86a5f5f649515eda1ea43bb4beef3c6
alexey-z-m <alexey.panda@gmail.com>
2022-06-05 14:52:53 +0300

    tableView created

--- hw13/hw13/ViewController.swift
+++ hw13/hw13/ViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ViewController.swift
-//  hw13
-//
-//  Created by Panda on 05.06.2022.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        var table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        title = "Настройки"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 }
 
