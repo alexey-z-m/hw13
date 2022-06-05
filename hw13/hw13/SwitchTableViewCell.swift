927fafa419104b65ac691eeb747d7deb8284d9ab
alexey-z-m <alexey.panda@gmail.com>
2022-06-05 15:24:59 +0300

    add switch cell

--- hw13/hw13/SwitchTableViewCell.swift
+++ hw13/hw13/SwitchTableViewCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-class SettingsTableViewCell: UITableViewCell {
-    static let identifier = "SettingsTableViewCell"
+class SwitchTableViewCell: UITableViewCell {
+    static let identifier = "SwitchTableViewCell"
     private let iconContainer: UIView = {
         let view = UIView()
         view.clipsToBounds = true
@@ -10,28 +10,9 @@ class SettingsTableViewCell: UITableViewCell {
         return view
     }()
     
-    private let notificationsContainer: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .systemRed
-        label.textColor = .white
-        label.textAlignment = .center
-        label.clipsToBounds = true
-        label.layer.masksToBounds = true
-        label.numberOfLines = 1
-        return label
-    }()
-    
     private let label: UILabel = {
         let label = UILabel()
         label.numberOfLines = 1
-        return label
-    }()
-    
-    private let subLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 1
-        label.textAlignment = .right
-        label.textColor = .systemGray
         return label
     }()
     
@@ -42,15 +23,19 @@ class SettingsTableViewCell: UITableViewCell {
         return imageView
     }()
     
+    private let _switch: UISwitch = {
+        let _switch = UISwitch()
+        _switch.onTintColor = .systemBlue
+        return _switch
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(label)
-        contentView.addSubview(notificationsContainer)
         contentView.addSubview(iconContainer)
-        contentView.addSubview(subLabel)
+        contentView.addSubview(_switch)
         iconContainer.addSubview(iconImageView)
         contentView.clipsToBounds = true
-        accessoryType = .disclosureIndicator
     }
     
     required init?(coder: NSCoder) {
@@ -60,21 +45,20 @@ class SettingsTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         let size = contentView.frame.size.height - 12
-        notificationsContainer.frame = CGRect(
-            x: contentView.frame.size.width - notificationsContainer.frame.size.width - 15,
-            y: 6,
-            width: size,
-            height: size
-        )
-        notificationsContainer.layer.cornerRadius = size / 2
         iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
         let imageSize: CGFloat = size/1.5
-        iconImageView.frame = CGRect(x: (size-imageSize)/2, y: (size-imageSize)/2, width: imageSize, height: imageSize)
-        subLabel.frame = CGRect(
-            x: contentView.frame.size.width - subLabel.frame.size.width - 10,
-            y: 0,
-            width: contentView.frame.size.width - 15 - iconContainer.frame.size.width - 10,
-            height: contentView.frame.size.height
+        iconImageView.frame = CGRect(
+            x: (size-imageSize)/2,
+            y: (size-imageSize)/2,
+            width: imageSize,
+            height: imageSize
+        )
+        _switch.sizeToFit()
+        _switch.frame = CGRect(
+            x: contentView.frame.size.width - _switch.frame.size.width - 20,
+            y: (contentView.frame.size.height - _switch.frame.size.height)/2,
+            width: _switch.frame.size.width,
+            height: _switch.frame.size.height
         )
         label.frame = CGRect(
             x: 25 + iconContainer.frame.size.width,
@@ -93,24 +77,15 @@ class SettingsTableViewCell: UITableViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         iconImageView.image = nil
-        subLabel.text = nil
         label.text = nil
         iconImageView.backgroundColor = nil
-        notificationsContainer.backgroundColor = nil
+        _switch.isOn = false
     }
     
-    public func configure(with model: SettingsOptons) {
+    public func configure(with model: SettingsSwitchOptons) {
         label.text = model.title
-        subLabel.text = model.detail
         iconImageView.image = model.icon
         iconContainer.backgroundColor = model.iconBackgroundColor
-        notificationsContainer.text = model.notifications
-        if model.notifications != nil {
-            notificationsContainer.backgroundColor = .systemRed
-        } else {
-            notificationsContainer.backgroundColor = nil
-        }
-        
+        _switch.isOn = model.isOn
     }
 }
-
--- hw13/hw13/ViewController.swift
+++ hw13/hw13/ViewController.swift
@@ -11,44 +11,156 @@ struct SettingsOptons {
 
 struct Sections {
     let title: String
-    let options: [SettingsOptons]
+    let options: [SettingsOptonsType]
+}
+
+struct SettingsSwitchOptons {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: ((String) -> Void)
+    let isOn: Bool
+}
+
+enum SettingsOptonsType {
+    case staticCell(model: SettingsOptons)
+    case switchCell(model: SettingsSwitchOptons)
 }
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
     private let tableView: UITableView = {
         var table = UITableView(frame: .zero, style: .grouped)
-        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: "SettingsTableViewCell")
+        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
         return table
     }()
     
     var models = [Sections]()
-    
-    var options: [SettingsOptons] = [
-        SettingsOptons(
+    var optionsFirst: [SettingsOptonsType] = [
+        .switchCell(model: SettingsSwitchOptons(
+            title: "Авиарежим",
+            icon: UIImage(systemName: "airplane"),
+            iconBackgroundColor: .systemOrange,
+            handler: { print("Нажата ячейка \($0)") },
+            isOn: false
+        )),
+        .staticCell(model: SettingsOptons(
             title: "Wi-Fi",
             icon: UIImage(systemName: "wifi"),
             iconBackgroundColor: .systemBlue,
             handler: { print("Нажата ячейка \($0)") },
             detail: "Не подключено",
             notifications: nil
-        ),
-        SettingsOptons(
+        )),
+        .staticCell(model: SettingsOptons(
             title: "Bluetooth",
             icon: UIImage(named: "bluetooth"),
             iconBackgroundColor: .systemBlue,
             handler: { print("Нажата ячейка \($0)") },
             detail: "Вкл.",
             notifications: nil
-        ),
-        SettingsOptons(
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Сотовая связь",
+            icon: UIImage(systemName: "antenna.radiowaves.left.and.right"),
+            iconBackgroundColor: .systemGreen,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Режим модема",
+            icon: UIImage(systemName: "personalhotspot"),
+            iconBackgroundColor: .systemGreen,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .switchCell(model: SettingsSwitchOptons(
+            title: "VPN",
+            icon: UIImage(named: "vpn"),
+            iconBackgroundColor: .systemBlue,
+            handler: { print("Нажата ячейка \($0)") },
+            isOn: true
+        ))
+    ]
+    
+    var optionsSecond: [SettingsOptonsType] = [
+        .staticCell(model: SettingsOptons(
+            title: "Уведомления",
+            icon: UIImage(systemName: "bell.badge.fill"),
+            iconBackgroundColor: .systemRed,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Звуки, тактильные сигналы",
+            icon: UIImage(systemName: "speaker.wave.3.fill"),
+            iconBackgroundColor: .systemRed,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Не беспокоить",
+            icon: UIImage(systemName: "moon.fill"),
+            iconBackgroundColor: .systemIndigo,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Экранное время",
+            icon: UIImage(systemName: "hourglass"),
+            iconBackgroundColor: .systemIndigo,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        ))
+    ]
+    
+    var optionsThird: [SettingsOptonsType] = [
+        .staticCell(model: SettingsOptons(
             title: "Основные",
             icon: UIImage(systemName: "gear"),
             iconBackgroundColor: .systemGray,
             handler: { print("Нажата ячейка \($0)") },
             detail: nil,
             notifications: "1"
-        )
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Пункт управления",
+            icon: UIImage(systemName: "switch.2"),
+            iconBackgroundColor: .systemGray,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Экран и яркость",
+            icon: UIImage(systemName: "textformat.size"),
+            iconBackgroundColor: .systemBlue,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Экран домой",
+            icon: UIImage(named: "homescreen"),
+            iconBackgroundColor: .systemBlue,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        )),
+        .staticCell(model: SettingsOptons(
+            title: "Универсальный доступ",
+            icon: UIImage(systemName: "figure.wave.circle"),
+            iconBackgroundColor: .systemBlue,
+            handler: { print("Нажата ячейка \($0)") },
+            detail: nil,
+            notifications: nil
+        ))
     ]
     
     override func viewDidLoad() {
@@ -62,19 +174,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func configure() {
-        models.append(Sections(title: "First", options: options))
+        models.append(Sections(title: "First", options: optionsFirst))
+        models.append(Sections(title: "Second", options: optionsSecond))
+        models.append(Sections(title: "Third", options: optionsThird))
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = models[indexPath.section].options[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(
-            withIdentifier: SettingsTableViewCell.identifier,
-            for: indexPath
-        ) as? SettingsTableViewCell else {
-            return UITableViewCell()
+        switch model.self {
+        case .staticCell(let model):
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsTableViewCell.identifier,
+                for: indexPath
+            ) as? SettingsTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+        case .switchCell(let model):
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: SwitchTableViewCell.identifier,
+                for: indexPath
+            ) as? SwitchTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
         }
-        cell.configure(with: model)
-        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -87,8 +213,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        switch model.self {
+        case .staticCell(let model):
+            model.handler(model.title)
+        case .switchCell(let model):
+            model.handler(model.title)
+        }
     }
-    
-    
 }
-
